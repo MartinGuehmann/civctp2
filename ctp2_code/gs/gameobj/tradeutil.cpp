927fcd0a00531123f94f3dee9d9d723ad0296a46
Martin Gühmann <guehmann@t-online.de>
2025-12-20 09:08:04 +0100

    Have trade routes again that need more than one caravan
    Basically, this is just reverting to the last release. In fact, it is not optimal, because that should be based on the freight costs on the way. But the exact calculation is slow, not very nice to open the trade  manager in the debug version.

--- ctp2_code/gs/gameobj/tradeutil.cpp
+++ ctp2_code/gs/gameobj/tradeutil.cpp
@@ -83,10 +83,12 @@ sint32 tradeutil_GetTradeValue(const sint32 owner, const Unit & destination, sin
 sint32 tradeutil_GetTradeDistance(const Unit &source, const Unit &destination)
 {
 	Path    path;
-	double  cost;
 
 #ifdef USE_PRECISETRADEROUTECALC
-// used to be tradeutil_GetAccurateTradeDistance which is exact but takes much longer to compute due to use of astar
+	float   cost; // Must be float for astar
+
+	// Used to be tradeutil_GetAccurateTradeDistance which is exact but takes much longer to compute due to use of astar
+	// Just open the trade manager in a late game in a debug version, that's not fun
 	if (g_theTradeAstar.FindPath // comparable to TradeRouteData::GeneratePath cost calculation
 	        (source.GetOwner(), source.RetPos(), destination.RetPos(), path, cost, FALSE)
 	   )
@@ -97,9 +99,12 @@ sint32 tradeutil_GetTradeDistance(const Unit &source, const Unit &destination)
 	return DISTANCE_UNKNOWN;
 
 #else // not USE_PRECISETRADEROUTECALC
-// used to be tradeutil_GetTradeDistance which is a very rough calculation but is quick due to avoiding astar
-	cost = 5.0 * // used to be g_theWorld->CalcTerrainFreightCost(source.RetPos()) * which was just returning 5 as a const in orig code: https://github.com/civctp2/civctp2/blob/d614fbdf705db334a4b45037bbcc735142d22016/ctp2_code/gs/world/wldgen.cpp#L1984-L1990
-	    static_cast<double>(source.RetPos().NormalizedDistance(destination.RetPos()));
+	// Used to be tradeutil_GetTradeDistance which is a very rough calculation but is quick due to avoiding astar
+	// Indeed this should not depend on the value at the source position
+	// cost = 5.0 * // used to be g_theWorld->CalcTerrainFreightCost(source.RetPos()) * which was just returning 5 as a const in orig code: https://github.com/civctp2/civctp2/blob/d614fbdf705db334a4b45037bbcc735142d22016/ctp2_code/gs/world/wldgen.cpp#L1984-L1990
+	// However, this still gives more than one catavan for trade routes
+	double cost = g_theWorld->CalcTerrainFreightCost(source.RetPos()) *
+	               static_cast<double>(source.RetPos().NormalizedDistance(destination.RetPos()));
 
 	return static_cast<sint32>(std::max<double>(tradeutil_GetNetTradeCosts(cost), 1.0));
 
